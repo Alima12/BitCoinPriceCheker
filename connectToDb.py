333bcacb3fcc5f56616d894ce9751c8efdb41a4b
alifrommac <aliali.ali1378@yahoo.com>
2021-03-25 20:59:53 +0430

    First version is Create

---
+++ connectToDb.py
@@ -0,0 +1,47 @@
+import sqlite3
+from datetime import datetime,timedelta
+
+connect = sqlite3.connect("dataprices.db")
+cursor = connect.cursor()
+def create():
+    q = """create table if not exists Prices(
+        symbol VACHAR,
+        buy INT,
+        sell INT,
+        sec DATETIME NOT NULL default CURRENT_TIMESTAMP
+    );"""
+    cursor.execute(q)
+
+
+def insert(buy,sell):
+    time = datetime.now().strftime("%y/%m/%d %H:%M:%S")
+    q = f"""INSERT INTO Prices VALUES("USD",{buy},{sell},'{time}');"""
+    cursor.execute(q)
+    connect.commit()
+
+
+def bestWeek(buy):
+    time = datetime.now() - timedelta(7)
+    time = time.strftime("%y/%m/%d %H:%M:%S")
+    q = f"""SELECT count(*) FROM Prices where sec > "{time}" and buy < {buy}; """
+    res = cursor.execute(q)
+    for item in res:
+        count = item[0]
+    if count == 0:
+        return True
+    return False
+
+def bestMonth(buy):
+    time = datetime.now() - timedelta(30)
+    time = time.strftime("%y/%m/%d %H:%M:%S")
+    q = f"""SELECT count(*) FROM Prices where sec > "{time}" and buy < {buy}; """
+    res = cursor.execute(q)
+    for item in res:
+        count = item[0]
+    if count == 0:
+        return True
+    return False
+
+create()
+
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import requests as req
+import sqlite3
+from connectToDb import insert,bestWeek,bestMonth
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+
+
+updater = Updater(token='1716430763:AAEuqY-YqLMfYO5PKU_lkJ73bJ4PvxLjGSs', use_context=True)
+bot = updater.bot
+def set_price():
+    url = "https://blockchain.info/ticker"
+    response = req.get(url)
+    response = response.json()
+    price = response["USD"]
+    buy_price = price["buy"]
+    if bestWeek(buy_price):
+        bot.send_message(88171378,f"بهترین قیمت در 7 روز اخیر : {buy_price}")
+        bot.send_message(-1001341117324,f"بهترین قیمت در 7 روز اخیر : {buy_price}")
+    if bestMonth(buy_price):
+        bot.send_message(88171378,f"بهترین قیمت در 30  روز اخیر : {buy_price}")
+        bot.send_message(-1001341117324,f"بهترین قیمت در 30  روز اخیر : {buy_price}")
+
+    insert(price["buy"],price["sell"])
+
+
+
+if __name__ == "__main__":
+    set_price()
