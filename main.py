e0d06f0215e0658bc203c93ece4e0c6a87a9fd04
alifrommac <aliali.ali1378@yahoo.com>
2021-03-29 17:07:58 +0430

    Sending Message with a better form

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from connectToDb import insert,bestWeek,bestMonth,bestThree,MostMonth,MostWeek,M
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
-
+from datetime import datetime
 
 updater = Updater(token='1716430763:AAEuqY-YqLMfYO5PKU_lkJ73bJ4PvxLjGSs', use_context=True)
 bot = updater.bot
@@ -20,20 +20,24 @@ def set_price():
     price = response["USD"]
     buy_price = price["buy"]
     message = []
+    title = f"📌قیمت کنونی بیتکوین=>   {buy_price} دلار \n"
+    message.append(title)
     if bestThree(buy_price):
-        message.append(f"کمترین قیمت در 3 روز گذشته : {buy_price}")
+        message.append(f"🔔 کمترین قیمت در 3 روز گذشته")
     if bestWeek(buy_price):
-        message.append(f"کمترین قیمت در  7 روز گذشته : {buy_price}")
+        message.append(f"🔔 کمترین قیمت در  7 روز گذشته")
     if bestMonth(buy_price):
-        message.append(f"کمترین قیمت در  30 روز گذشته : {buy_price}")
+        message.append(f"🔔 کمترین قیمت در  30 روز گذشته")
     if MostThree(buy_price):
-        message.append(f"بیشترین قیمت در 3 روز گذشته : {buy_price}")
+        message.append(f"🔔 بیشترین قیمت در 3 روز گذشته")
     if MostWeek(buy_price):
-        message.append(f"بیشترین قیمت در  7 روز گذشته : {buy_price}")
+        message.append(f"🔔 بیشترین قیمت در  7 روز گذشته")
     if MostMonth(buy_price):
-        message.append(f"بیشترین قیمت در  30 روز گذشته : {buy_price}")
+        message.append(f"🔔 بیشترین قیمت در  30 روز گذشته")
 
-    if len(message) > 0:
+    if len(message) > 1:
+        __time = datetime.now().strftime("%H:%M:%S")
+        message.append(__time)
         msg = '\n'.join(message)
         send_notif(msg)
     insert(price["buy"],price["sell"])
